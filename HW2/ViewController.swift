b0d3afec7474444235673715625e1b99b49a3c0c
Egeberk Özberk <egeberk@gmail.com>
2022-05-28 10:07:08 +0300

    Second test

--- HW2/ViewController.swift
+++ HW2/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-// Test Commit
+// Test Commit Second
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
